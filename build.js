e8c8faadba0ed20e70ac2bf77c7230230ddabeb8
nebobyeoli <nikkilover17@naver.com>
2020-09-04 15:24:22 +0900

    setSecret

--- build.js
+++ build.js
@@ -1,7 +1,11 @@
+const { setSecret } = require('@actions/core');
+
 require('dotenv').config();
+require('@actions/core');
 
 // var sec = 'ABCD ' + process.env.test_secret.slice(0, 4);
 // console.log(`test_secret: ${sec}`);
 // console.log(`length: ${process.env.test_secret.length}`);
 
+setSecret('test_secret');
 console.log(process.env);
