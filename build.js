73936e7327553274e7c3f705f0b33fe60fee32d1
nebobyeoli <nikkilover17@naver.com>
2020-09-04 15:20:00 +0900

    process env

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-var sec = 'ABCD ' + process.env.test_secret.slice(0, 4);
-console.log(`length: ${process.env.test_secret.length}`);
-console.log(`test_secret: ${sec}`);
+// var sec = 'ABCD ' + process.env.test_secret.slice(0, 4);
+// console.log(`test_secret: ${sec}`);
+// console.log(`length: ${process.env.test_secret.length}`);
 
 console.log(process.env);
