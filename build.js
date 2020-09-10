e2a5314b9d15528aaa8ae9b0144e5d6ed5be82ba
nebobyeoli <nikkilover17@naver.com>
2020-09-10 16:51:41 +0900

    not good

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 // const yenv = require('yenv');
 // const env = yenv('.github/workflows/main.yml');
-core.exportVariable('envVar', secrets.TEST_SECRET );
+core.exportVariable('envVar', '${{ secrets.TEST_SECRET }}');
 
 console.log(`::set-env name=APP_NAME::$(cat Dockerfile | grep APP_NAME= | head -n 1 | grep -o '".*"' | sed 's/"//g')`);
 console.log('test_secret: ' + process.env.test_secret);
