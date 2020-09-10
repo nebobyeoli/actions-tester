9f9779d6f1756ea55ff0cdbc18082e69ca735020
nebobyeoli <nikkilover17@naver.com>
2020-09-10 16:05:50 +0900

    print

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 require('@actions/core');
-const yenv = require('yenv');
+// const yenv = require('yenv');
+// const env = yenv('.github/workflows/main.yml');
 
-const env = yenv('.github/workflows/main.yml');
-
-// console.log(process.env);
 console.log('test_secret: ' + process.env.test_secret);
+console.log('.');
+console.log(process.env);
 // console.log('yenv: ' + env.build.steps.env.yEnv);
