123a41948f5f2582f1d0e6b64e5a27c62722c75e
nebobyeoli <nikkilover17@naver.com>
2020-09-10 16:13:24 +0900

    envVar

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 const core = require('@actions/core');
 // const yenv = require('yenv');
 // const env = yenv('.github/workflows/main.yml');
-core.exportVariable('envVar', 'Val');
+core.exportVariable('envVar', '${{ secrets.TEST_SECRET }}');
 
 console.log('test_secret: ' + process.env.test_secret);
-console.log(process.env.envVar);
+console.log('envVar: ' + process.env.envVar);
 console.log('.');
 console.log(process.env);
 // console.log('yenv: ' + env.build.steps.env.yEnv);
