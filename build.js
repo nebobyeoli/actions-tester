2540634855209ee1979e57fc3229da87bd7699f5
nebobyeoli <nikkilover17@naver.com>
2020-09-10 17:25:16 +0900

    HAH

--- build.js
+++ build.js
@@ -5,7 +5,10 @@ core.exportVariable('envVar', '${{ secrets.TEST_SECRET }}');
 
 // console.log(`::set-env name=APP_NAME::$(cat Dockerfile | grep APP_NAME= | head -n 1 | grep -o '".*"' | sed 's/"//g')`);
 const se = process.env.test_secret;
+if (se == 12345678) console.log('number');
+if (se == '12345678') console.log('string');
 console.log('test_secret: ' + se);
+
 console.log('envVar: ' + process.env.envVar);
 console.log('.');
 console.log(process.env);
