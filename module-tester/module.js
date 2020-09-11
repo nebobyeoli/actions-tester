dce024b40b9325e64ffc7edb3d7d5e2b3ccc1082
nebobyeoli <nikkilover17@naver.com>
2020-09-11 22:03:34 +0900

    module-tester

---
+++ module-tester/module.js
@@ -0,0 +1,18 @@
+// import * as puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
+
+const lunge = async () => {
+    puppeteer.launch({headless:false}).then(async browser => {
+        await browser.newPage();
+        await browser.close();
+    });
+    console.log('lunge has wooked');
+}
+
+const sayHello = () => {
+    lunge();
+    console.log('Hello World!');
+}
+
+module.exports.sayHello = sayHello;
+// export { sayHello as sayHello };
---
+++ module-tester/scripts.js
@@ -0,0 +1,4 @@
+// import * as mod from './module.js';
+const mod = require('./module.js');
+
+mod.sayHello();
