529b90b974855c4fb5ebc0c2774ee565220fd3b0
nebobyeoli <nikkilover17@naver.com>
2020-09-15 12:58:59 +0900

    Get tools-build

--- express-wrap-tester/script.js
+++ express-wrap-tester/script.js
@@ -1,8 +1,9 @@
-const test_secret = process.env.test_secret;
+const { test_secret } = require('../tools/build.js');
 
-if (test_secret == 12345678) setInner('number');
+setInner('<br>');
+if (test_secret == 12345678) addInner('number');
 if (test_secret == '12345678') addInner('string');
 console.log('test_secret: ' + test_secret);
 
 function setInner(newInner) { document.getElementById('isit').innerHTML = newInner; }
-function addInner(newInner) { document.getElementById('isit').innerHTML += '\n' + newInner; }
+function addInner(newInner) { document.getElementById('isit').innerHTML += '<br>' + newInner; }
